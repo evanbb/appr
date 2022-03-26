a9fdfab926ab234337fb382d84f48d405195aa75
Evan Bremer <evanbb@gmail.com>
2022-03-25 20:15:17 -0500

    add api

--- apps/todo-api/rollup.config.js
+++ apps/todo-api/rollup.config.js
@@ -1,11 +1,44 @@
-import ts from '@rollup/plugin-typescript'
+import ts from '@rollup/plugin-typescript';
+import nodemon from 'nodemon';
+
+const isWatch = process.env.ROLLUP_WATCH === 'true';
+const api = function api({ outputDir = 'dist/index.js' }) {
+  let mon = null;
+  let restarting = false;
+
+  return {
+    name: 'rollup-plugin-api',
+
+    options() {
+      // start the api
+      if (!mon) {
+        console.log('Starting the app!');
+        mon = nodemon(outputDir);
+
+        mon.on('restart', function () {
+          restarting = true;
+        });
+
+        mon.on('exit', function () {
+          if (!restarting) {
+            process.kill(process.pid);
+            return;
+          }
+
+          restarting = false;
+        });
+      }
+    },
+  };
+};
 
 export default {
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist'
-    },
-    plugins: [
-        ts()
-    ]
-}
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+  },
+  plugins: [ts(), isWatch ? api({ entry: 'dist/index.js' }) : null].filter(
+    Boolean
+  ),
+};
--- apps/todo-api/src/index.ts
+++ apps/todo-api/src/index.ts
@@ -1,1 +1,9 @@
-export default 'i am api'
+import express from 'express';
+
+const app = express();
+
+app.listen(8000, () => {
+  console.error('Listening on port 8000');
+});
+
+export default 42;
